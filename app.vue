c3071e486bee0f3bd619537ca0d1e5cf69b87862
Vyacheslav <vyacheslav@MacBook-Pro-Vyacheslav.local>
2022-04-22 18:05:30 +0400

    init

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+    <Space/>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Space from './Space.vue'
+
+export default defineComponent({
+  components: {
+    Space,
+  }
+})
+</script>
+
+
+<style scoped>
+body, html {
+  background: black;
+  padding: 0;
+  margin: 0;
+}
+</style>
