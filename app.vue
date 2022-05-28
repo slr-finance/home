c6d87e1c9c0e2514fdac02fa8bc49aa105051772
Vyacheslav <vyacheslav@MacBook-Pro-Vyacheslav.local>
2022-05-29 02:01:43 +0400

    feat: update home bg

--- app.vue
+++ app.vue
@@ -1,23 +1,63 @@
 <template>
-    <Space/>
+  <home-bg/>
+  <div class="text-box">
+    <div class="heading">Slr.Finance</div>
+    <div class="button-wrapper">
+        <a href="https://app.slr.finance" class="button">Launch app</a>
+        <a href="https://docs.slr.finance" class="button">Docs</a>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Space from './Space.vue'
+import HomeBg from './components/HomeBg.vue'
 
 export default defineComponent({
   components: {
-    Space,
+    HomeBg,
   }
 })
 </script>
 
 
-<style scoped>
+<style>
 body, html {
   background: black;
   padding: 0;
   margin: 0;
+}
+
+.text-box {
+  text-align: left;
+  left: 0;
+  top: 20%;
+  z-index: 2;
+  font-size: 60px;
+  font-family: 'Raleway', sans-serif;
+  letter-spacing: 7px;
+  word-spacing: 12px;
+  text-transform: uppercase;
+  color:rgb(238, 238, 238);
+}
+.button-wrapper {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 20px;
+  flex-direction: row;
+}
+.button {
+  display: inline-block;
+  font-size: 16px;
+  font-weight: bold;
+  padding: 15px;
+  margin-left:20px;
+  margin-right:20px;
+  word-spacing: normal;
+  letter-spacing: 5px;
+  border: 1px solid white;
+  background: black;
+  cursor: pointer;
 }
 </style>
---
+++ components/HomeBg.vue
@@ -0,0 +1,158 @@
+<template>
+  <canvas ref="canvasRef" style="width: 100%; height: 500px;"/>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue'
+import { useResizeObserver, useRafFn } from '@vueuse/core'
+
+const fovY = 30
+const gridColor = '#1B1B1B'
+const rayColor = '#2B2B2B'
+
+const interpolate = (a:number, b:number, d: number) => a * (1 - d) + b * d
+
+const drawStaticLayer = (ctx:CanvasRenderingContext2D, width: number, height: number, centerX:number, fovX:number, perspectiveRate: number) => {
+  const rate = height / width
+
+  const bgGradient = ctx.createRadialGradient(centerX, centerX, 0, centerX, centerX, centerX * 1.1);
+  bgGradient.addColorStop(0.1, '#101010');
+  bgGradient.addColorStop(0.8, 'black');
+
+  ctx.fillStyle = bgGradient;
+  ctx.setTransform(1,0,0, rate,0,0)
+  ctx.fillRect(0,0,width,width)
+  ctx.setTransform(1,0,0, 1, 0,0)
+
+  ctx.strokeStyle = gridColor
+  ctx.lineWidth = 1
+
+  ctx.beginPath()
+
+  for(let x = -centerX; x <= centerX; x+=fovX) {
+    ctx.moveTo(x + centerX, 0)
+    ctx.lineTo((x * perspectiveRate) + centerX, height)
+  }
+
+  /// create horizontal lines
+  for(let index = 0; index <= Math.ceil(height / fovY); index+=1) {
+    const y = index * fovY * index / 10
+    ctx.moveTo(0, y)
+    ctx.lineTo(width, y)
+  }
+
+  ctx.stroke()
+}
+
+const drawRay = (ctx:CanvasRenderingContext2D, width: number, height: number, fovX: number, perspectiveRate:number, offset:number, col:number, time: number) => {
+  const centerX = Math.ceil(width / 2)
+
+  const d = ((time + offset) % 5000) / 5000
+  const tailOffset = 0.2
+
+  const d1 = Math.max(Math.min(d + tailOffset, 1) - tailOffset, 0) / (1 - tailOffset)
+  const d2 = Math.max(d - tailOffset, 0) / (1 - tailOffset)
+
+  let x = -centerX + fovX * col
+
+  const x0 = x + centerX
+  const x1 = (x * perspectiveRate) + centerX
+  const y0 = 0
+  const y1 = height
+
+  const interpolateX0 = interpolate(x1, x0, d1)
+  const interpolateX1 = interpolate(x1, x0, d2)
+  const interpolateY0 = interpolate(y1, y0, d1)
+  const interpolateY1 = interpolate(y1, y0, d2)
+  
+  ctx.beginPath()
+  ctx.moveTo(interpolateX0, interpolateY0)
+  ctx.lineTo(interpolateX1, interpolateY1)
+
+  const gradient = ctx.createLinearGradient(
+    0, interpolateY0,
+    0, interpolateY1,
+  );
+
+  gradient.addColorStop(0, rayColor)
+  gradient.addColorStop(0.2, rayColor)
+  gradient.addColorStop(1, 'transparent')
+
+  ctx.strokeStyle = gradient
+  ctx.stroke()
+}
+
+const drawVignette = (ctx:CanvasRenderingContext2D, width: number, height: number, centerX: number) => {
+  const rate = height / width
+
+  var bgGradient = ctx.createRadialGradient(centerX, centerX, 0, centerX, centerX, centerX * 1.1);
+  bgGradient.addColorStop(0.8, 'transparent');
+  bgGradient.addColorStop(1, 'black');
+
+  ctx.fillStyle = bgGradient;
+  ctx.setTransform(1,0,0, rate, 0,0)
+  ctx.fillRect(0,0,width,width)
+  ctx.setTransform(1,0,0, 1,0,0)
+}
+
+export default defineComponent({
+  setup() {
+    const canvasRef = ref(null as null|HTMLCanvasElement)
+    let width = 0
+    let height = 0
+    let ctx:null|CanvasRenderingContext2D = null
+    let canvasEl:null|HTMLCanvasElement = null
+    let cols = 0
+    let perspectiveRate = 3.5
+    let fovX = 120
+    let centerX = Math.ceil(width / 2)
+
+    useResizeObserver(canvasRef, ([entry]) => {
+      if (!canvasEl || !ctx) {
+        return
+      }
+
+      width = entry.contentRect.width
+      height = entry.contentRect.height
+      centerX = Math.ceil(width / 2)
+      fovX = width / 15
+      cols = Math.ceil(width / fovX)
+
+      canvasEl.width = width
+      canvasEl.height = height
+    })
+
+    useRafFn(() => {
+      if (!canvasEl || !ctx) {
+        return
+      }
+
+      ctx.clearRect(0, 0, width, height)
+      drawStaticLayer(ctx, width, height, centerX, fovX, perspectiveRate)
+      const time = Date.now()
+      // первые n и последние n колонок можно пропустить, т.к. они за пределами видимости из-за затемнения
+      const skipCol = 4
+      const startCol = skipCol 
+      const endCol = cols - skipCol
+
+      for (let index = startCol; index < endCol; index++) {
+        drawRay(ctx, width, height, fovX, perspectiveRate, 2050 * index, index, time)
+      }
+
+      drawVignette(ctx, width, height, centerX)
+    })
+
+    onMounted(() => {
+      canvasEl = canvasRef.value
+
+      if (!canvasEl) {
+        throw new Error()
+      }
+
+      ctx = canvasEl.getContext('2d', { alpha: false })
+    })
+
+    return { canvasRef }
+  },
+})
+</script>
