69ae5f58d2d337e5730f6bad3721eda379c2951b
Timur Ramazanov <timramazov94@gmail.com>
2022-06-25 11:04:29 +0400

    feat: add roadmap slider

---
+++ components/home/RoadmapSlider.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="relative pt-ui-page-header-spacing pb-80">
+    <h2 class="mb-32 font-title text-gray text-60">
+      WHERE ARE WE IN <span class="text-white">THE ROADMAP?</span>
+    </h2>
+    <div class="flex mb-[100px]">
+      <div class="text-[34px] font-title flex-shrink-0 mr-64">
+        <h4 class="text-gray">Current status:</h4>
+        <h4 class="text-white">Rersricted mainnet</h4>
+      </div>
+      <p class="text-20 text-gray">Full Mainnet begins with successful completion of the final launch phase. This begins when the community have signalled their confidence in the software via on-chain governance votes and all remaining temporary limits have been removed.</p>
+    </div>
+    <div class="absolute w-full h-2 bg-white" />
+    <div class="flex justify-between items-center pt-32">
+      <button class="w-64 h-64 rounded-full flex items-center justify-center bg-gray-800 shrink-0" @click="changeScroll('left')">
+        <ui-icon name="arrow-pixel-to" :size="20" class="text-white transform rotate-90"/>
+      </button>
+      <div ref="wrapper" class="flex space-x-[76px] overflow-x-auto snap-x absolute left-[72px] right-[72px] items-center h-[218px]">
+        <div class="flex flex-col items-center shrink-0 relative snap-start">
+          <ui-icon class="absolute -top-[80px]" name="flag" size="54" />
+          <span class="text-gray text-18">Current status</span>
+          <span class="text-18 text-white">Rersricted mainnet</span>
+        </div>
+        <div class="flex flex-col items-center shrink-0 relative snap-start">
+          <ui-icon class="absolute -top-[52px]" name="star-pixel" />
+          <span class="text-gray text-18">Current status</span>
+          <span class="text-18 text-white">Rersricted mainnet</span>
+        </div>
+        <div class="flex flex-col items-center shrink-0 relative snap-start">
+          <ui-icon class="absolute -top-[52px]" name="star-pixel" />
+          <span class="text-gray text-18">Current status</span>
+          <span class="text-18 text-white">Rersricted mainnet</span>
+        </div>
+        <div class="flex flex-col items-center shrink-0 relative snap-start">
+          <ui-icon class="absolute -top-[52px]" name="star-pixel" />
+          <span class="text-gray text-18">Current status</span>
+          <span class="text-18 text-white">Rersricted mainnet</span>
+        </div>
+        <div class="flex flex-col items-center shrink-0 relative snap-start">
+          <ui-icon class="absolute -top-[52px]" name="star-pixel" />
+          <span class="text-gray text-18">Current status</span>
+          <span class="text-18 text-white">Rersricted mainnet</span>
+        </div>
+        <div class="flex flex-col items-center shrink-0 relative snap-start">
+          <ui-icon class="absolute -top-[52px]" name="star-pixel" />
+          <span class="text-gray text-18">Current status</span>
+          <span class="text-18 text-white">Rersricted mainnet</span>
+        </div>
+      </div>
+      <button class="w-64 h-64 rounded-full flex items-center justify-center bg-gray-800 shrink-0" @click="changeScroll('right')">
+        <ui-icon name="arrow-pixel-to" :size="20" class="text-white transform -rotate-90"/>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted } from 'vue'
+import UiIcon from '~~/components/ui/UiIcon/UiIcon.vue'
+import AppLandingTitle from '../App/AppLandingTitle.vue';
+
+const ITEM_WIDTH = 162;
+const GAP = 76;
+
+export default defineComponent({
+  name: 'home-roadmap-slider',
+  components: { UiIcon, AppLandingTitle },
+  setup() {
+    const wrapper = ref<HTMLDivElement>(null)
+    let idx = 0
+
+    const changeScroll = (direction: 'right' | 'left') => {
+      const visibleElements = Math.floor(wrapper.value.clientWidth / (ITEM_WIDTH + GAP))
+  
+      if (direction === 'right') {
+        idx = Math.min(
+          wrapper.value.children.length - 1,
+          idx === 0 ? visibleElements + 1 : idx + 1
+        )
+      } else {
+        idx = Math.max(
+          0,
+          idx === wrapper.value.children.length - 1 ? idx - visibleElements - 1 : idx - 1
+        )
+      }
+
+      wrapper.value.children[idx]?.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    return {
+      wrapper,
+      idx,
+      changeScroll
+    }
+  },
+})
+</script>
